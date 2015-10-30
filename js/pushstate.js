4c7b3a3b48eefe517e7d7ecc74ce26f317c138dc
hcpark <pchuri@gmail.com>
2015-10-30 10:30:06 +0900

    Add the event for refresh.

--- js/pushstate.js
+++ js/pushstate.js
@@ -2,3 +2,6 @@ $(document).on("pjax:end", function() {
   window.postMessage({ action: "lgtm:refresh" }, "*");
 });
 
+$( document ).ajaxComplete(function() {
+    window.postMessage({ action: "lgtm:refresh" }, "*");
+})
